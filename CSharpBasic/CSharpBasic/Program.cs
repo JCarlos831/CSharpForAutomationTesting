a1408dfba1b5eb184e383950c951ba16ad258cfd
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-07-19 15:07:30 -0400

    Working with Class and Objects

---
+++ CSharpBasic/CSharpBasic/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CSharpBasic
+{
+    internal class Program
+    {
+        // Creation of an object for the Program class
+        Program p = new Program();
+        
+        // Entry point of the program
+        public static void Main(string[] args)
+        {
+            
+            TestClass1 class1 = new TestClass1();
+            
+            class1.SetValue(20);
+            class1.GetValue();
+            class1.SetValue(50);
+            
+            TestClass1 class2 = new TestClass1();
+            class2.SetValue(30);
+            class2.GetValue();
+            
+            class1.GetValue();
+            
+            Console.Read();
+        }
+    }
+}
---
+++ CSharpBasic/CSharpBasic/TestClass1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CSharpBasic
+{
+    public class TestClass1
+    {
+
+        private int i = 0;
+
+        public void SetValue(int value)
+        {
+            i = value;
+        }
+
+        public void GetValue()
+        {
+            Console.WriteLine("The value of i is: " + i);
+        }
+
+        public void TestCase1(string result)
+        {
+            Console.WriteLine("Executing Test Case 1 " + result);
+        }
+        
+        public void TestCase2(string result)
+        {
+            Console.WriteLine("Executing Test Case 2 " + result);
+        }
+    }
+}
